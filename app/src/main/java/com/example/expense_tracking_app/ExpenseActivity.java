8ec921daf5573a3d53a6a7db1b828d8746d30c48
Victor Pan <41593388+LordArugula@users.noreply.github.com>
2023-02-09 21:43:59 -0800

    implemented date picker

--- app/src/main/java/com/example/expense_tracking_app/ExpenseActivity.java
+++ app/src/main/java/com/example/expense_tracking_app/ExpenseActivity.java
@@ -1,7 +1,9 @@
 package com.example.expense_tracking_app;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Calendar;
 
 public class ExpenseActivity extends AppCompatActivity {
     public static final int EXPENSE_ERROR_ID = -2;
@@ -20,7 +23,7 @@ public class ExpenseActivity extends AppCompatActivity {
     private static final String TAG = ExpenseActivity.class.getSimpleName();
 
     private EditText nameText;
-    private EditText dateText;
+    private TextView dateText;
     private EditText costText;
     private TextView costSymbolText;
     private EditText categoryText;
@@ -28,6 +31,8 @@ public class ExpenseActivity extends AppCompatActivity {
     private EditText notesText;
 
     private int id;
+
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +46,40 @@ public class ExpenseActivity extends AppCompatActivity {
         categoryText = findViewById(R.id.expense_category);
         reasonText = findViewById(R.id.expense_reason);
         notesText = findViewById(R.id.expense_notes);
-
         Button deleteButton = findViewById(R.id.delete_expense);
 
         Intent intent = getIntent();
 
         id = intent.getIntExtra(getString(R.string.EXTRA_EXPENSE_ID), EXPENSE_ERROR_ID);
         assert id != EXPENSE_ERROR_ID;
-
         if (id == EXPENSE_NEW) {
             deleteButton.setVisibility(View.INVISIBLE);
         }
 
+        populateForm(intent);
+
+        initializeDatePicker();
+    }
+
+    private void initializeDatePicker() {
+        datePickerDialog = new DatePickerDialog(this);
+
+        datePickerDialog.setOnDateSetListener((datePicker, year, month, day) -> {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year, month, day);
+            dateText.setText(DateFormat.format("MM/dd/yyyy", calendar));
+        });
+        
+        dateText.setOnClickListener(view -> datePickerDialog.show());
+        dateText.setOnFocusChangeListener((view, hasFocus) -> {
+            if (hasFocus) {
+                datePickerDialog.show();
+            }
+        });
+
+    }
+
+    private void populateForm(Intent intent) {
         String name = intent.getStringExtra(getString(R.string.EXTRA_EXPENSE_NAME));
         String date = intent.getStringExtra(getString(R.string.EXTRA_EXPENSE_DATE));
         double cost = intent.getDoubleExtra(getString(R.string.EXTRA_EXPENSE_COST), 0);
